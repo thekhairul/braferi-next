d4a1d1a092e66c67ebca9f77de9d6dbc4828bc3d
thekhairul <thekhairul.office@gmail.com>
2022-09-05 15:49:09 +0600

    product gallery with zoom effect

---
+++ components/ProductGallery.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import ReactImageMagnify from "react-image-magnify";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Navigation, Thumbs } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+function ProductGallery({ images }) {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  return (
+    <div>
+      <Swiper spaceBetween={10} thumbs={{ swiper: thumbsSwiper }} modules={[Thumbs]} className="product-gallery mb-2">
+        {images.map((image) => (
+          <SwiperSlide key={image.id}>
+            <ReactImageMagnify
+              {...{
+                smallImage: {
+                  alt: "Wristwatch by Ted Baker London",
+                  isFluidWidth: true,
+                  src: image.url
+                },
+                largeImage: {
+                  src: image.url,
+                  width: 1200,
+                  height: 1200
+                },
+                enlargedImagePosition: 'over',
+                enlargedImageClassName: 'max-w-none object-cover',
+                className: 'rounded-3xl overflow-hidden'
+              }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        navigation
+        spaceBetween={10}
+        slidesPerView={4}
+        modules={[Navigation, Thumbs]}
+        className="product-gallery-nav"
+      >
+        {images.map((image) => (
+          <SwiperSlide key={image.id}>
+            <img src={image.url} alt="" className="rounded-xl" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default ProductGallery;
--- components/ProductPreview.jsx
+++ components/ProductPreview.jsx
@@ -1,13 +1,14 @@
 import { BsCartPlus } from "react-icons/bs";
 import { TbCurrencyTaka } from "react-icons/tb";
+import ProductGallery from "./ProductGallery";
 
 function ProductPreview({product}) {
     return (
         <div className="bg-white flex flex-wrap">
-            <div className="w-full md:w-2/5 p-4">
-                <img src={product.featuredImage.url} alt={product.title} className="rounded-3xl shadow-lg"/>
+            <div className="w-full md:w-1/2 p-4">
+                <ProductGallery images={product.images}/>
             </div>
-            <div className="w-full md:w-3/5 p-4">
+            <div className="w-full md:w-1/2 p-4">
                 <h3 className="text-dark font-semibold text-lg mb-2">{product.title}</h3>
                 <p className='mb-4'>
                     <span className="text-accent font-bold inline-flex items-center">
@@ -15,7 +16,7 @@ function ProductPreview({product}) {
                     {~~product.variants[0].priceV2.amount}
                     </span>
                 </p>
-                <p className='text-dark mb-6'>{product.description}</p>
+                {/* <p className='text-dark mb-6'>{product.description}</p> */}
                 <button
                     className="bg-accent hover:bg-accent-dark text-white font-bold px-6 py-3 rounded-full inline-flex items-center gap-2"
                 >
